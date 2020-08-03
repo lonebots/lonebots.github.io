815da7ad6040214d9c31c929fedd9af6b36c3396
lonebots <jishnusurajila@gmail.com>
2020-08-03 23:32:06 +0530

    statis page build

---
+++ precache-manifest.35018a748725fb2ef5df17768d1e604c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72ca52df7c7b92e173c41be110865bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f29272580bee31bd0e7",
+    "url": "/static/css/main.f40ed49c.chunk.css"
+  },
+  {
+    "revision": "f432dc503a35e741ce26",
+    "url": "/static/js/2.34f924ce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.34f924ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f29272580bee31bd0e7",
+    "url": "/static/js/main.36baf8ed.chunk.js"
+  },
+  {
+    "revision": "b648f39298b5e628d00c",
+    "url": "/static/js/runtime-main.67e0fbf0.js"
+  },
+  {
+    "revision": "35e8c30a6ccb65929a2d59466c280241",
+    "url": "/static/media/p-pic1.35e8c30a.jpg"
+  }
+]);
